22236d205a820c8f76d9c931ab7ac9fdc1e22dbe
Arthur <nashcassano8@gmail.com>
2025-12-07 14:49:29 -0300

    implementando authcontroller para o login e cadastro de usuarios

---
+++ petshop/Controller/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using petshop.DTO;
+using petshop.Model;
+using petshop.Repository;
+
+namespace petshop.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : Microsoft.AspNetCore.Mvc.Controller
+{
+    private readonly IUserRepository _repository;
+
+    public AuthController(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDTO login)
+    {
+        var user = await _repository.GetByEmail(login.Email);
+        
+        if (user == null)
+            return Unauthorized();
+        
+        if(user.Password != login.Password && user.Email != login.Email)
+            return Unauthorized();
+        
+        return Ok(user);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserDTO user)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest();
+
+        var response = new User
+        {
+            Email = user.Email,
+            Password = user.Password,
+            Name = user.Name,
+            Telefone = user.Telefone,
+        };
+        
+        await _repository.Create(response);
+        return Ok(user);
+    }
+}
---
+++ petshop/DTO/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace petshop.DTO;
+
+public class UserDTO
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    
+    [EmailAddress]
+    [Required]
+    public string Email { get; set; }
+    
+    [Required]
+    [MaxLength(11)]
+    [MinLength(11)]
+    public string Telefone { get; set; } 
+    
+    [Required]
+    [MaxLength(100)]
+    [MinLength(6)]
+    public string Password { get; set; }    
+}
