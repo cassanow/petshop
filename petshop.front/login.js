3eb6114a478b51c3ecbef112ccb2247909ade983
Arthur Cassiano Vitor <arthur.vitor@ik.com.br>
2025-12-08 14:37:45 -0300

    alterando js do login

--- petshop.front/login.js
+++ petshop.front/login.js
@@ -1,10 +1,26 @@
-    document.getElementById('loginForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Login realizado com sucesso!');
-        });
+document.getElementById("confirmar").addEventListener("click", async () => {
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
 
-        document.querySelectorAll('.social-btn').forEach(btn => {
-            btn.addEventListener('click', function() {
-                alert('Login com ' + this.textContent + ' em desenvolvimento');
-            });
-        });
+    try {
+        const response = await fetch("https://petzcare-api.onrender.com/api/Auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email: email, password: senha })
+        })
+
+        const data = await response.json
+
+        if(response.ok){
+            console.log("deu bom", data)
+        }
+        else{
+            console.log("deu ruim", data)
+        }
+    }
+
+    catch(err){
+        console.log(err)
+    }
+    
+})
