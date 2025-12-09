3a551d4f840bd3f858bb7e69f94aa317f6b31374
Arthur Cassiano Vitor <arthur.vitor@ik.com.br>
2025-12-09 14:20:26 -0300

    a

--- petshop.front/register.js
+++ petshop.front/register.js
@@ -52,9 +52,10 @@ function traduzirErro(msg) {
     if (msg.includes("Email") && msg.includes("valid")) return "Formato de e-mail inválido.";
     if (msg.includes("Telefone") && msg.includes("required")) return "O telefone é obrigatório.";
     if (msg.includes("Telefone") && msg.includes("minimum length")) return "O telefone deve ter exatamente 11 dígitos.";
+    if (msg.includes("Telefone") && msg.includes("maximum length")) return "A senha deve ter pelo menos 6 caracteres.";
     if (msg.includes("Password") && msg.includes("required")) return "A senha é obrigatória.";
     if (msg.includes("Password") && msg.includes("minimum length")) return "A senha deve ter pelo menos 6 caracteres.";
-    if (msg.includes("Password") && msg.includes("maximum length")) return "A senha deve ter pelo menos 6 caracteres.";
+
 
     return msg; 
 }
